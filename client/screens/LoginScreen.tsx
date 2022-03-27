a078c080266f9106d8e93901ebc5a96a05470afb
StritanS <tristan.duong@efrei.net>
2022-03-27 22:51:27 +0200

    little modif

--- client/screens/LoginScreen.tsx
+++ client/screens/LoginScreen.tsx
@@ -4,32 +4,55 @@ import Colors from '../constants/Colors';
 import { Entypo } from '@expo/vector-icons';
 import React from 'react';
 
+
 import { Text, View } from '../components/Themed';
 
 export default function Screen() {
 
-	const [text, setText] = React.useState('Change the title of the page');
+	const [Id, setText] = React.useState('');
+	const [Password, setTittle] = React.useState('');
+
+
+
 
 	return (
 		<View style={styles.container}>
 			<Text style={styles.title}>Login</Text>
 			<TextInput
 				style={{
-					height: 80,
-					width: '100%',
+					height: 30,
+					width: '75%',
 					borderColor: 'gray',
 					borderWidth: 1,
 					color: 'white',
 					fontSize: 20
 				}}
 				onChangeText={(input) => setText(input)}
-				placeholder="Type here to replace the text above!"
-				value={text}
+				placeholder="Id"
+				value={Id}
 			/>
+			<TextInput
+				style={{
+					height: 30,
+					width: '75%',
+					borderColor: 'gray',
+					borderWidth: 1,
+					color: 'white',
+					fontSize: 20
+				}}
+				onChangeText={(input) => setTittle(input)}
+				placeholder="Password"
+				value={Password}
+			/>
+			<a href='MdpOublie'>
+				<Text style={styles.textright}>Mot de passe oublié</Text>
+
+			</a>
 			<View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+			<Text style={styles.title}>Sign up</Text>
 			<View style={styles.sameLine}>
 				<a href='SpotifyLogin'>
-					<Text style={styles.title}>Login with Spotify</Text>
+					<Text style={styles.title}>Sign up with Spotify</Text>
 
 
 
@@ -48,8 +71,16 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		alignItems: 'center',
-		justifyContent: 'center',
+		justifyContent: 'space-evenly',
 		backgroundColor: Colors.dark_violet,
+
+
+	},
+	textright: {
+		fontSize: 10,
+		fontWeight: 'bold',
+		marginLeft: 175,
+
 	},
 	title: {
 		fontSize: 20,
