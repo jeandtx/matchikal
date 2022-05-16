08a2618dede546a890061499b26606e81adf6c2c
jeandtx <jean.doutriaux@gmail.com>
2022-05-16 11:22:05 +0200

    fetch data:tracks of user from spotify

--- client/screens/MyMatchikals.tsx
+++ client/screens/MyMatchikals.tsx
@@ -1,31 +1,77 @@
 import { StyleSheet } from 'react-native';
-
+import Bouton from '../components/Bouton';
+import React from 'react';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import axios from 'axios';
 
 export default function MyMatchikals() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/MyMatchikals.tsx" />
-    </View>
-  );
+
+	function filterData(data: JSON) {
+
+		return {
+			att1: '',
+			att2: {
+				att2_1: '',
+				att2_2: '',
+			},
+			att3: '',
+		}
+	}
+
+	function saveData(filteredData: Object) {
+		// if (data is not already saved)
+		return
+	}
+
+	let array: Array<Object> = [];
+	function getSongs(next: string) {
+		console.log("getSongs");
+		if (window.localStorage.getItem("token") != null) {
+			axios({
+				method: 'get',
+				url: next == '' ? 'https://api.spotify.com/v1/me/tracks' : next,
+				headers: {
+					'Authorization': 'Bearer ' + window.localStorage.getItem('token')
+				}
+			}).then(response => {
+				array.push(...response.data.items);
+				if (response.data.next) {
+					getSongs(response.data.next);
+				}
+				else {
+					console.log(array);
+					return array;
+				}
+			});
+		} else {
+			console.log("User is not connected");
+		}
+	}
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>MyMatchikals</Text>
+			<Bouton text='Call API' test={() => console.log(getSongs(''))} />
+			<View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+			<EditScreenInfo path="/screens/MyMatchikals.tsx" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: 'bold',
+	},
+	separator: {
+		marginVertical: 30,
+		height: 1,
+		width: '80%',
+	},
 });
