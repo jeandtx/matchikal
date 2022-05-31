af40ede0404ceee2d7889a565afddf4106979b31
jeandtx <jean.doutriaux@gmail.com>
2022-05-31 12:09:56 +0200

    resolved conflicts

--- client/screens/NotFoundScreen.tsx
+++ client/screens/NotFoundScreen.tsx
@@ -1,36 +1,55 @@
-import { StyleSheet, TouchableOpacity } from 'react-native';
+import { StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
 import { Text, View } from '../components/Themed';
 import { RootStackScreenProps } from '../types';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:19007');
 
 export default function NotFoundScreen({ navigation }: RootStackScreenProps<'NotFound'>) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>This screen doesn't exist.</Text>
-      <TouchableOpacity onPress={() => navigation.replace('Root')} style={styles.link}>
-        <Text style={styles.linkText}>Go to home screen!</Text>
-      </TouchableOpacity>
-    </View>
-  );
+
+	function sendMessage() {
+		socket.emit('message', 'Hello from the client');
+	}
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>This screen doesn't exist.</Text>
+			<TouchableOpacity onPress={() => navigation.replace('Root')} style={styles.link}>
+				<Text style={styles.linkText}>Go to home screen!</Text>
+			</TouchableOpacity>
+			<Text style={styles.title}>Server test zone</Text>
+			<TextInput
+				style={{
+					backgroundColor: '#fff',
+					padding: 10,
+				}}
+				placeholder="Message ..." />
+			<TouchableOpacity onPress={() => sendMessage()} style={styles.link}>
+				<Text style={styles.linkText}>Send Message</Text>
+			</TouchableOpacity>
+
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-  linkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		padding: 20,
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: 'bold',
+	},
+	link: {
+		marginTop: 15,
+		paddingVertical: 15,
+	},
+	linkText: {
+		fontSize: 14,
+		color: '#2e78b7',
+	},
 });
--- server/index.js
+++ server/index.js
@@ -10,7 +10,6 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:19006/',
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
     },
 });
